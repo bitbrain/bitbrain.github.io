3b3a7b3e8264af9608947383b7ada387f6fae6f7
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-09-09 00:13:01 +0200

    Blog works now locally with Ajax

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+
+
+function animate() {
   $('.logo').css("letterSpacing", "3px");
   $('.logo').stop(false, true).animate({
       letterSpacing: "0px"
@@ -23,4 +25,41 @@ $(document).ready(function() {
       backgroundColor: "rgb(39, 39, 39)",
     }, 200);
   });
+}
+
+function runAjax() {
+  var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+    // Make sure that all clicked links that link to your internal website
+    // don't just reload the page but execute a History.pushState call
+    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+        e.preventDefault();
+        content = jQuery('.page-content');
+        pathname = this.pathname;
+        History.pushState({}, "", pathname);
+    });
+
+    // Catch all History stateChange events
+    History.Adapter.bind(window, 'statechange', function(){
+        var State = History.getState();
+
+        // Load the new state's URL via an Ajax Call
+        $.get(State.url, function(data){
+            // Replace the "<title>" tag's content
+            document.title = "bit.brain";
+
+            // Replace the content of the main container (.content)
+            // If you're using another div, you should change the selector
+            $('.page-content').html($(data).filter('.page-content').html());
+            $('.page-content').css('display', 'block');
+            // If you're using Google analytics, make sure the pageview is registered!
+            //_gaq.push(['_trackPageview', State.url]);
+            animate();
+        });
+    });
+}
+
+$(document).ready(function() {
+  animate();
+  runAjax();
 });
